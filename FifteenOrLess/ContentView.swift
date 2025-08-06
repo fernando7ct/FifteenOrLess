f2440338a1706c4f04cbd2791f0004cfdc6fc23e
Fernando Caudillo Tafoya <fcaudillotafoya@icloud.com>
2025-08-05 20:17:53 -0500

    Initial Commit

---
+++ FifteenOrLess/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FifteenOrLess
+//
+//  Created by Fernando Caudillo Tafoya on 8/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FifteenOrLess/FifteenOrLessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FifteenOrLessApp.swift
+//  FifteenOrLess
+//
+//  Created by Fernando Caudillo Tafoya on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct FifteenOrLessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
